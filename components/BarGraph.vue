ff490de0c4a6404f886d203c8c21e8259e909740
Jonas Brauer <jonasbrauer@seznam.cz>
2023-03-20 09:40:53 +0100

    Disable graph animation, fix label shadow

--- components/BarGraph.vue
+++ components/BarGraph.vue
@@ -5,12 +5,12 @@
                 v-for="tag in displayedTags" :to="`/tags/${tag.tag.id}`"
                 @click="toggleSelected(tag.tag.id)"
                 class="row-wrapper"
-                :style="`color: ${light_or_dark(tag.tag.color) ? '#2b2b2b': '#faf9f9'}; text-shadow: 0px 0px 2px #fff;`"
+                :style="`color: ${light_or_dark(tag.tag.color) ? '#2b2b2b': '#faf9f9'}; text-shadow: 0px 0px 2px ${tag.tag.color}, 0px 0px 2px ${tag.tag.color}, 0px 0px 2px ${tag.tag.color};`"
             >
                 <p class="label" :style="`background-color: ${tag.tag.color}`">{{ tag.tag.name }}</p>
                 <div class="bar-wrapper">
                     <p class="bar" 
-                        :style="`width: ${tag.tag.id === selected ? 100 : (tag.sum / max * loaded)}%; background-color: ${tag.tag.color}`">
+                        :style="`width: ${tag.tag.id === selected ? 100 : (tag.sum / max) * 100}%; background-color: ${tag.tag.color}`">
                         <Price
                             :amount="tag.sum"
                             :currency="tag.transactions[0].currency"
@@ -36,8 +36,8 @@ export default {
 
     data() {
         return {
-            loaded: 0,
             selected: -1,
+            max: 0,
         }
     },
 
@@ -48,21 +48,24 @@ export default {
             }
             return this.tags.filter(t => t.tag.id === this.selected);
         },
-        max() {
-            let max = 0;
-            Object.values(this.tags).forEach(t => {
-                if (t.sum > max) {
-                    max = t.sum;
-                }
-            })
-            return max
-        },
+    },
+
+    created() {
+        let max = 0;
+        Object.values(this.tags).forEach(t => {
+            if (t.sum > max) { max = t.sum; }
+        })
+        this.max = max;
     },
 
     methods: {
-        light_or_dark(color) {
-            if (color) {
-                return color && parseInt(color.replace('#', ''), 16) > 12000000;
+        light_or_dark(colorHex) {
+            if (colorHex) {
+                let color = colorHex.startsWith("#") ? colorHex.replace("#", "") : colorHex;
+                const r = parseInt(color.substring(0, 2), 16); // hexToR
+                const g = parseInt(color.substring(2, 4), 16); // hexToG
+                const b = parseInt(color.substring(4, 6), 16); // hexToB
+                return (r*0.299 + g*0.587 + b*0.114) > 186;
             }
             return false
         },
@@ -78,14 +81,8 @@ export default {
             if (tag.tag.id === this.selected) {
                 return 100;
             }
-            return tag.sum / max * loaded
+            return tag.sum / max * 100
         }
     },
-
-    created() {
-        setTimeout(() => {
-            this.loaded = 100;
-        }, 100)
-    }
 }
 </script>
