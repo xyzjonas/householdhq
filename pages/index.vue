2167774d67ec694001f16be879ff2bb8fd11fadc
Jonas Brauer <jonasbrauer@seznam.cz>
2023-04-21 19:04:38 +0200

    Fix conditional

--- pages/index.vue
+++ pages/index.vue
@@ -172,7 +172,7 @@ export default {
         })
         .then(res => {
           this.allTransactions = res.data.filter(trans => {
-            return trans.target.isDisponible
+            return trans.target.isDisponible || trans.source.isDisponible
           })
         })
         .finally(() => {
