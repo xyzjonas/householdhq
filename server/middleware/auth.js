7b967017f591836a8999cc49dd25a68dd1b480cc
Jonas Brauer <jonasbrauer@seznam.cz>
2023-04-25 08:34:07 +0200

    Uncomment auth middleware *duh

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -5,30 +5,30 @@ const pubKey = process.env.AUTH0_PUBKEY
 
 
 export default defineEventHandler((event) => {
-  // const token = event.node.req.headers.authorization;
-  // if (!String(event.node.req.url).startsWith("/api")) {
-  //   console.debug(`${String(event.node.req.url)}: this is an unprotected endpoint!`)
-  // } else if (token) {
-  //   try {
-  //     let authorization = token.split(' ');
-  //     if (authorization[0] !== 'Bearer') {
-  //       throw createError({ statusCode: 401, statusMessage: 'Invalid auth type.' })
-  //     } else if (!authorization[1]) {
-  //       throw createError({ statusCode: 401, statusMessage: 'Empty token' })
-  //     } else {
-  //       console.info(authorization);
-  //       console.info(`secret: ${pubKey}`)
-  //       const decoded = jwt.verify(authorization[1], pubKey, {
-  //         algorithms: ['RS256'],
-  //       })
-  //       console.info("!!!!!!!")
-  //       console.info(decoded)
-  //     }
-  //   } catch (err) {
-  //     console.error(err);
-  //     throw createError({ statusCode: 403, statusMessage: String(err) });
-  //   }
-  // } else {
-  //   throw createError({ statusCode: 401, statusMessage: 'Authorization is missing.' })
-  // } 
+  const token = event.node.req.headers.authorization;
+  if (!String(event.node.req.url).startsWith("/api")) {
+    console.debug(`${String(event.node.req.url)}: this is an unprotected endpoint!`)
+  } else if (token) {
+    try {
+      let authorization = token.split(' ');
+      if (authorization[0] !== 'Bearer') {
+        throw createError({ statusCode: 401, statusMessage: 'Invalid auth type.' })
+      } else if (!authorization[1]) {
+        throw createError({ statusCode: 401, statusMessage: 'Empty token' })
+      } else {
+        console.info(authorization);
+        console.info(`secret: ${pubKey}`)
+        const decoded = jwt.verify(authorization[1], pubKey, {
+          algorithms: ['RS256'],
+        })
+        console.info("!!!!!!!")
+        console.info(decoded)
+      }
+    } catch (err) {
+      console.error(err);
+      throw createError({ statusCode: 403, statusMessage: String(err) });
+    }
+  } else {
+    throw createError({ statusCode: 401, statusMessage: 'Authorization is missing.' })
+  } 
 })
