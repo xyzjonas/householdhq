9024e5f6efe9a985c01adc1c16117c1b122beead
Jonas <jonasbrauer@seznam.cz>
2024-05-29 14:40:04 +0200

    add data-migration script

--- prisma/migrations/20240529085252_transacted_at/data-migration.ts
+++ prisma/migrations/20240529085252_transacted_at/data-migration.ts
@@ -4,10 +4,16 @@ const prisma = new PrismaClient();
 
 async function main() {
 
-  const transactions = await prisma.$transaction(async tx => tx.transaction.findMany());
+  // (1) Fill in the transactedAt timestamp
+  let transactions = await prisma.$transaction(async tx => tx.transaction.findMany());
   for (const trans of transactions) {
     const i = transactions.indexOf(trans)
+
     console.info(`Updating transaction ${i + 1}/${transactions.length}`)
+    if (trans.trasactedAt) {
+      continue
+    }
+
     await prisma.$transaction(async (tx) => {  
       await tx.transaction.update({
         where: { id: trans.id },
@@ -19,35 +25,54 @@ async function main() {
     })
   }
 
-//   await prisma.$transaction(async (tx) => {
-//     const tags = await tx.tag.findMany();
-//     const transactions = await tx.transaction.findMany({
-//       include: { tags: true },
-//     });
-//     for (const tag of tags) {
-//       const newCategory = await tx.category.create({
-//         data: {
-//           name: tag.name,
-//           color: tag.color,
-//           description: tag.description,
-//           icon: tag.icon,
-//         },
-//       });
+  // (2) Copy over: tags -> categories
+  await prisma.$transaction(async (tx) => {
+    const tags = await tx.tag.findMany();
+    for (const tag of tags) {
+      const newCategory = await tx.category.upsert({
+        where: {
+          name: tag.name,
+        },
+        create: {
+          name: tag.name,
+          color: tag.color,
+          description: tag.description,
+          icon: tag.icon,
+        },
+        update: {}
+      });
 
-//       await tx.transaction.updateMany({
-//         where: {
-//           tags: {
-//             some: {
-//               id: tag.id,
-//             },
-//           },
-//         },
-//         data: {
-//           categoryId: newCategory.id,
-//         },
-//       });
-//     }
-//   });
+      await tx.transaction.updateMany({
+        where: {
+          tags: {
+            some: {
+              id: tag.id,
+            },
+          },
+        },
+        data: {
+          categoryId: newCategory.id,
+        },
+      });
+    }
+  });
+
+  // (3) Reset the previous legacy created timestamp (since it is auto-updated)
+  transactions = await prisma.$transaction(async tx => tx.transaction.findMany());
+  for (const trans of transactions) {
+    const i = transactions.indexOf(trans)
+
+    console.info(`Resetting created timestamp ${i + 1}/${transactions.length}`)
+    await prisma.$transaction(async (tx) => {  
+      await tx.transaction.update({
+        where: { id: trans.id },
+        data: {
+          created: trans.trasactedAt ?? undefined,
+        },
+      });
+    })
+  }
+
 }
 
 main()
